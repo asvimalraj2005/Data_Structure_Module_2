873a45d5df0f7c6db7f28756594af5a18c39f9c5
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-15 20:31:14 +0530

    Problem4.py

---
+++ Binary_Search/Lab/Additional/Problem4.py
@@ -0,0 +1,5 @@
+Given an integer array A of size N.
+
+If we store the sum of each triplet of the array A in a new list, then find the Bth smallest element among the list.
+
+NOTE: A triplet consists of three elements from the array. Let's say if A[i], A[j], A[k] are the elements of the triplet then i < j < k.
