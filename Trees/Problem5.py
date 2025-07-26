1e141f634704d5e0ce58d08493a9e6ced72a5c31
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-26 22:11:56 +0530

    Problem5.py

--- Trees/Problem5.py
+++ Trees/Problem5.py
@@ -12,6 +12,5 @@ class Solution:
         if not root:
             return 0
         left_depth = self.maxDepth(root.left)
-        right_depth = self.maxDepth(root.right)
-        
+        right_depth = self.maxDepth(root.right)        
         return max(left_depth, right_depth) + 1
