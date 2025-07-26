4a89374cb3a866b4ed288f4f436483cd2b775b1d
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-07-26 22:11:38 +0530

    Problem5.py

--- Trees/Problem5.py
+++ Trees/Problem5.py
@@ -13,4 +13,5 @@ class Solution:
             return 0
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
+        
         return max(left_depth, right_depth) + 1
