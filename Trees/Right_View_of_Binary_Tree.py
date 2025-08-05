3760c12ff984474270ce26ea483c68493c0aab7c
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-08-05 23:08:27 +0530

    Right_View_of_Binary_Tree.py

---
+++ Trees/Right_View_of_Binary_Tree.py
@@ -0,0 +1,3 @@
+Given a binary tree of integers denoted by root A. Return an array of integers representing the right view of the Binary tree.
+
+Right view of a Binary Tree is a set of nodes visible when the tree is visited from Right side.
